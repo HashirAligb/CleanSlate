3f848688b26fcbc4505ae0e503119982af4bb61f
HashirAligb <HASHIRALI.ALI21@qmail.cuny.edu>
2025-07-04 23:34:09 -0400

    just put a /greet POST endpoint

--- server/src/index.ts
+++ server/src/index.ts
@@ -19,6 +19,16 @@ app.get("/", (req: Request, res: Response) => {
   res.send("CleanSlate API is live with TypeScript!");
 });
 
+app.post("/greet", (req: Request, res: Response) => {
+  const { name } = req.body;
+
+  if (!name) {
+    return res.status(400).json({ error: "Name is required" });
+  }
+
+  res.json({ message: `Hello, ${name}!` });
+});
+
 app.listen(5000, () => {
   console.log("Server listening on port 5000");
 });
